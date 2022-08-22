404d6c8048f9ee73cffabeb85725c0b29fd25759
evgen_vuzlov <evuzlov@gmail.com>
2022-08-22 10:33:49 +0300

    add homework_5

---
+++ homework-5/index.js
@@ -0,0 +1,20 @@
+const number1 = parseInt(prompt(`enter your first number`));
+const operation = prompt(`enter operation(+-*/)`);
+const number2 = parseInt(prompt(`enter your second number`));
+if (isNaN(number1) || isNaN(number2)) {
+    alert(`not correct number`)}
+else if (operation !== '/'&&
+    operation !=='*'&&
+    operation !=='-'&&
+    operation !=='+') {
+    alert(`This operation is not exist`)}
+else if (operation === '*') {
+    result = number1 * number2} 
+else if (operation === '+') {
+    result = number1 + number2}
+else if (operation === '-') {
+    result = number1 - number2}
+else if (operation === '/') {
+    result = number1 / number2}
+alert(`your result is ${result}`)
+
