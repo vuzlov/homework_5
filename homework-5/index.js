dc249f53843a6809d5cc2816dc894f93172e8375
evgen_vuzlov <evuzlov@gmail.com>
2022-08-23 13:46:49 +0300

    fixed bugs

--- homework-5/index.js
+++ homework-5/index.js
@@ -1,23 +1,18 @@
 const number1 = parseInt(prompt(`enter your first number`));
 const operation = prompt(`enter operation(+-*/)`);
 const number2 = parseInt(prompt(`enter your second number`));
-const result = ''
-if (isNaN(number1) || isNaN(number2)) {
-    alert(`not correct number`)}
-else if (operation !== '/'&&
-    operation !=='*'&&
-    operation !=='-'&&
-    operation !=='+') {
-    alert(`This operation is not exist`)}
-else if (operation === '*') {
-    result = number1 * number2
-    alert(`your result is ${result}`)}
-else if (operation === '+') {
-    result = number1 + number2
-    alert(`your result is ${result}`)}
-else if (operation === '-') {
-    result = number1 - number2
-    alert(`your result is ${result}`)}
-else if (operation === '/') {
-    result = number1 / number2
-    alert(`your result is ${result}`)}
+if (isNaN(number1) || isNaN(number2))
+    alert(`not correct number`)
+else if (operation !== '/' &&
+    operation !== '*' &&
+    operation !== '-' &&
+    operation !== '+')
+    alert(`This operation is not exist`)
+else if (operation === '*')
+    alert(`your result is ${number1 * number2}`)
+else if (operation === '+')
+    alert(`your result is ${number1 + number2}`)
+else if (operation === '-')
+    alert(`your result is ${number1 - number2}`)
+else if (operation === '/')
+    alert(`your result is ${number1 / number2}`)
