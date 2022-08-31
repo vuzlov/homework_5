9d52fb378d26434a64352bd67cfece18ee99424a
evgen_vuzlov <evuzlov@gmail.com>
2022-08-31 23:07:33 +0300

    homework 8

---
+++ homework-8/index.js
@@ -0,0 +1,27 @@
+let store = {
+    name : apchi,
+    address : {
+        country : ukraine,
+        city : odesa,
+        street : MalayaArnoutskaya,
+        houseNumber : 17
+    }
+}
+store.clients ={}
+store.clients = {
+    'Bohdan Onatskyi': {
+    quantity: 50,
+    bonuses :63.},
+    'Kirill Makhnovskyi' : {
+    quantity: 40,
+    bonuses :53.},
+    'Zhykhariev Anatolii': {
+    quantity: 20,
+    bonuses :38.},
+    'Olha Kovalova': {
+    quantity: 13,
+    bonuses :20.},
+    'Elena Kuzmicheva': {
+    quantity: 8,
+    bonuses :24.}
+}
